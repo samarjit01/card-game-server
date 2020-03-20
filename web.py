224f787ccf94cacb49d0ebdb3d74ab417d81da1e
tejaswinicp2509 <tejaswinicp25>
2020-03-20 17:56:16 +0530

    Added GET method

--- web.py
+++ web.py
@@ -1,6 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
+all_services ={"result":[{"id":0 , "name":"bubble_sort", "id":1 , "name":"merge_sort"}]}
+
+
 @app.route('/')
 def index():
   return "Hello world "
+  
+@app.route('/api/getAllServices')
+def getAllservices():
+  return all_services
+
